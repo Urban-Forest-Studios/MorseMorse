64178b4de3b98965e4453e7ca3ea67085595e45c
Luca Thompson <luca+github@urbanforest.dev>
2023-03-02 00:41:22 -0600

    wee snaw

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -16,13 +16,14 @@ public class GameManager : MonoBehaviour
     private int i;
 
     public bool isGrid;
+    public bool audioPlaying = false;
     public int questionCount;
     public Text questiontext;
     public Text answertext;
     public Text countText;
     private Lessons lessons;
     public List<QuestionElement> questions;
-    public List<QuestionElement> _unansweredQuestions;
+    public List<QuestionElement> answeredQuestions;
     public GameObject completeScreen;
     public GameObject correct;
     public GameObject incorrect;
@@ -85,12 +86,12 @@ public class GameManager : MonoBehaviour
         { '=', "-...-" },
         { '.', ".-.-.-" },
         { ';', "-..-." },
-        { '\'', ".----."},
+        { '\'', ".----." },
         { ')', "-.--.-" },
         { '+', ".-.-." },
         { '@', ".--.-." },
-        { '!', "---."}
-    }; 
+        { '!', "---." }
+    };
 
     private void Start()
     {
@@ -101,7 +102,7 @@ public class GameManager : MonoBehaviour
 
         questionCount = questions.Count;
         countText.text = questionCount.ToString();
-        if (_unansweredQuestions == null || _unansweredQuestions.Count == 0) _unansweredQuestions = questions;
+        answeredQuestions = new List<QuestionElement>();
         if (lessons.LessonsLessons[i].Type == "grid")
         {
             isGrid = true;
@@ -116,74 +117,69 @@ public class GameManager : MonoBehaviour
             type.SetActive(true);
             GetRandomQuestion();
         }
+
         questiontext.text = currentQuestion.String;
         answertext.text = "";
     }
-    
+
 
     private void GetRandomQuestion()
     {
-        var randomQuestionIndex = Random.Range(0, _unansweredQuestions.Count);
-        currentQuestion = _unansweredQuestions[randomQuestionIndex];
+        var randomQuestionIndex = Random.Range(0, questions.Count);
+        while (answeredQuestions.Contains(questions[randomQuestionIndex]))
+        {
+            randomQuestionIndex = Random.Range(0, questions.Count);
+        }
+        currentQuestion = questions[randomQuestionIndex];
         PlayMorseCode(ToMorse(currentQuestion.String));
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
     private void GetGridQuestions()
     {
-        var randomQuestionIndex = Random.Range(0, _unansweredQuestions.Count);
+        /*lsnJson = File.ReadAllText("Assets/questions/lessons.json");
+        lessons = Lessons.FromJson(lsnJson);
+        questions = lessons.LessonsLessons[i].Questions;*/
+        int randomQuestionIndex = Random.Range(0, questions.Count);
         var index = 0;
-        //buttons no longer added dynamically
-        /*if (btns.Count == null || btns.Count == 0)
+        currentQuestion = questions[randomQuestionIndex];
+        if (answeredQuestions.Contains(currentQuestion))
         {
-            while (btns.Count < 4)
+            while (answeredQuestions.Contains(currentQuestion))
             {
-                GameObject createdButtonItem = Instantiate(btn);
-                //createdButtonItem.GetComponent<ButtonEntryData>().choice.text = ToMorse(gridQuestions[index].String);
-                createdButtonItem.transform.SetParent(grid.transform);
-                btns.Add(createdButtonItem);
+                currentQuestion = questions[randomQuestionIndex];
             }
-        }*/
-        foreach (var b in GameObject.FindGameObjectsWithTag("dynamicButton"))
-        {
-            b.GetComponent<Image>().color = defaultcolor;
         }
-        var correctAnswerIndex = Random.Range(0, _unansweredQuestions.Count);
-        currentQuestion = _unansweredQuestions[correctAnswerIndex];
-        while (index < 3)
+        gridQuestions.Add(currentQuestion);
+        while (gridQuestions.Count < 4)
         {
             randomQuestionIndex = Random.Range(0, questions.Count);
-            var candidate = questions[randomQuestionIndex]; 
-            if (!gridQuestions.Contains(candidate) || currentQuestion.String != candidate.String)
+            var candidate = questions[randomQuestionIndex];
+            if (!gridQuestions.Contains(candidate) && !currentQuestion.Equals(candidate))
             {
                 gridQuestions.Add(candidate);
-                index++;
             }
-            gridQuestions.Add(currentQuestion);
-            gridQuestions.Shuffle();
+
         }
+        gridQuestions.Shuffle();
 
         index = 0;
-        /*GameObject createdButtonItem = Instantiate(btn);
-        createdButtonItem.transform.SetParent(grid.transform);*/
-        correctAnswerIndex = gridQuestions.IndexOf(currentQuestion);
+        var correctAnswerIndex = gridQuestions.IndexOf(currentQuestion);
         foreach (var createdButtonItem in btns)
         {
-            if (index != correctAnswerIndex)
+            createdButtonItem.GetComponent<ButtonEntryData>().choice.text = ToMorse(gridQuestions[index].String);
+            if (index == correctAnswerIndex)
             {
-                createdButtonItem.GetComponent<ButtonEntryData>().answer = false;
-                createdButtonItem.GetComponent<ButtonEntryData>().choice.text = ToMorse(gridQuestions[index].String);
+                createdButtonItem.GetComponent<ButtonEntryData>().answer = true;
             }
             else
             {
-                createdButtonItem.GetComponent<ButtonEntryData>().choice.text = ToMorse(gridQuestions[index].String);
-                createdButtonItem.GetComponent<ButtonEntryData>().answer = true;
-                currentQuestion = gridQuestions[correctAnswerIndex];
-                Debug.Log(gridQuestions[correctAnswerIndex].String);
-                PlayMorseCode(ToMorse(gridQuestions[correctAnswerIndex].String));
+                createdButtonItem.GetComponent<ButtonEntryData>().answer = false;
             }
-
             index++;
         }
+        PlayMorseCode(ToMorse(currentQuestion.String));
+        gridQuestions.Clear();
 
     }
 
@@ -215,36 +211,39 @@ public class GameManager : MonoBehaviour
         {
             b.GetComponent<Image>().color = transparent;
         }
+
         questionCount--;
         countText.text = questionCount.ToString();
         if (questionCount != 0)
         {
             correct.SetActive(true);
-            _unansweredQuestions.RemoveAt(questions.IndexOf(currentQuestion));
+            answeredQuestions.Add(currentQuestion);
             answertext.text = "";
             StartCoroutine(NextQuestion(1));
         }
-        else if (questionCount == 0 || questionCount < 0)
+        else if (questionCount <= 0)
         {
-            
+
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
-    
+
     public void Incorrect()
     {
-        
+
         gridQuestions.Clear();
         incorrect.SetActive(false);
         foreach (var b in GameObject.FindGameObjectsWithTag("dynamicButton"))
         {
             b.GetComponent<Image>().color = transparent;
         }
+
         countText.text = questionCount.ToString();
         incorrect.SetActive(true);
         answertext.text = "";
         StartCoroutine(NextQuestion(1));
     }
+
     public void Submit()
     {
         incorrect.SetActive(false);
@@ -267,7 +266,7 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         gridQuestions.Clear();
-        if(isGrid) GetGridQuestions();
+        if (isGrid) GetGridQuestions();
         else GetRandomQuestion();
         questiontext.text = currentQuestion.String;
     }
@@ -290,59 +289,78 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
-    
-    
+
+
     //MORSE CODE PLAYER
-    public float dotFrequency = 550.0f;  // Frequency of a dot in Hz
-    public float dashFrequency = 550.0f;  // Frequency of a dash in Hz
-    public float sampleRate = 44100.0f;  // Audio sample rate in Hz
-    public float dotDuration = 0.1f;  // Duration of a dot in seconds
-    public float dashDuration = 0.3f;  // Duration of a dash in seconds
-    public float silenceDuration = 0.1f;  // Duration of the silence between dots and dashes in seconds
-    public float cspaceSilenceDuration = 0.3f;  // Duration of the silence between characters in seconds
+    public float dotFrequency = 550.0f; // Frequency of a dot in Hz
+    public float dashFrequency = 550.0f; // Frequency of a dash in Hz
+    public float sampleRate = 44100.0f; // Audio sample rate in Hz
+    public float dotDuration = 0.1f; // Duration of a dot in seconds
+    public float dashDuration = 0.3f; // Duration of a dash in seconds
+    public float silenceDuration = 0.1f; // Duration of the silence between dots and dashes in seconds
+    public float cspaceSilenceDuration = 0.3f; // Duration of the silence between characters in seconds
     public AudioSource source;
 
-    public void PlayMorseCode(string morseCode) {
+    public void PlayMorseCode(string morseCode)
+    {
         StartCoroutine(PlayMorseCodeCoroutine(morseCode));
     }
 
-    IEnumerator PlayMorseCodeCoroutine(string morseCode) {
+    IEnumerator PlayMorseCodeCoroutine(string morseCode)
+    {
+        audioPlaying = true;
         int numSamplesDot = Mathf.RoundToInt(dotDuration * sampleRate);
         int numSamplesDash = Mathf.RoundToInt(dashDuration * sampleRate);
         int numSamplesSilence = Mathf.RoundToInt(silenceDuration * sampleRate);
-        
 
-        foreach (char c in morseCode) {
-            if (c == '.') {
+
+        foreach (char c in morseCode)
+        {
+            if (c == '.')
+            {
                 PlayAudio(numSamplesDot);
                 yield return new WaitForSeconds(dotDuration);
-            } else if (c == '-') {
+            }
+            else if (c == '-')
+            {
                 PlayAudio(numSamplesDash);
                 yield return new WaitForSeconds(dashDuration);
-            } else if (c == ' ') {
+            }
+            else if (c == ' ')
+            {
                 yield return new WaitForSeconds(cspaceSilenceDuration);
-            } else if (c == '/')
+            }
+            else if (c == '/')
             {
                 yield return new WaitForSeconds(0.5f);
             }
+
             yield return new WaitForSeconds(silenceDuration);
         }
+
+        audioPlaying = false;
     }
 
     void PlayAudio(int smpl)
     {
         int sampleFreq = 44000;
         float frequency = 550;
- 
+
         float[] samples = new float[smpl];
-        for(int i = 0; i < samples.Length; i++)
+        for (int i = 0; i < samples.Length; i++)
         {
-            samples[i] = Mathf.Sin(Mathf.PI*2*i*frequency/sampleFreq);
+            samples[i] = Mathf.Sin(Mathf.PI * 2 * i * frequency / sampleFreq);
         }
+
         AudioClip ac = AudioClip.Create("Test", samples.Length, 1, sampleFreq, false);
         ac.SetData(samples, 0);
         gameObject.GetComponent<AudioSource>().clip = ac;
         gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    public void RepeatAudioPrompt()
+    {
+        if(!audioPlaying) PlayMorseCode(ToMorse(currentQuestion.String));
     }
 }
 
@@ -354,9 +372,7 @@ public static class Extensions
     {
         for (int i = values.Count - 1; i > 0; i--) {
             int k = rand.Next(i + 1);
-            T value = values[k];
-            values[k] = values[i];
-            values[i] = value;
+            (values[k], values[i]) = (values[i], values[k]);
         }
     }
 }
